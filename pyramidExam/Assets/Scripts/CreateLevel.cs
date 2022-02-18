bf62b67bd37701077560fbfe12e7537ec2d5c5f2
GGreyKnight <damiankarwowski9@gmail.com>
2022-02-18 02:29:50 +0100

    Save4
    Working random rooms!!!!!

--- pyramidExam/Assets/Scripts/CreateLevel.cs
+++ pyramidExam/Assets/Scripts/CreateLevel.cs
@@ -51,7 +51,7 @@ public class CreateLevel : MonoBehaviour
             }
             else
             {
-                walls[i].transform.localScale = new Vector3(roomY - wallThickness, wallHeight, wallThickness);
+                walls[i].transform.localScale = new Vector3(roomY + wallThickness, wallHeight, wallThickness);
             }
         }
 
@@ -61,8 +61,9 @@ public class CreateLevel : MonoBehaviour
             walls[1].transform.position = new Vector3(roomX / 2, wallCenter, 0);
             walls[1].transform.Rotate(0, 90, 0);
             walls[2].transform.position = new Vector3(0, wallCenter, -roomY / 2);
+            walls[2].transform.Rotate(0, 180, 0);
             walls[3].transform.position = new Vector3(-roomX / 2, wallCenter, 0);
-            walls[3].transform.Rotate(0, -90, 0);
+            walls[3].transform.Rotate(0, 270, 0);
         }
         
     }
@@ -73,8 +74,70 @@ public class CreateLevel : MonoBehaviour
         Debug.Log(randomWall);
         if(walls[randomWall].transform.localScale.x>=1)
         {
-            int randomPlaceOnWall = Random.Range(0, Mathf.RoundToInt(walls[randomWall].transform.localScale.x));
+            float randomPlaceOnWall = Random.Range(0, Mathf.RoundToInt(walls[randomWall].transform.localScale.x));
             Debug.Log(randomPlaceOnWall);
+            GameObject door = Instantiate(doorPrefab);
+            door.transform.rotation = walls[randomWall].transform.rotation;
+            door.transform.position = new Vector3(walls[randomWall].transform.position.x, door.transform.position.y, walls[randomWall].transform.position.z);
+            if(door.transform.position.x == 0)
+            {
+                //move door to its destination, we add .5f because doors ofsett (center is at .5f)
+                door.transform.position += new Vector3(-walls[randomWall].transform.localScale.x/2 + randomPlaceOnWall + .5f, 0, 0);
+                if (randomPlaceOnWall == 0)
+                {
+                    door.transform.position += new Vector3(wallThickness, 0, 0);
+                }
+                else if (randomPlaceOnWall == Mathf.RoundToInt(walls[randomWall].transform.localScale.x))
+                {
+                    door.transform.position += new Vector3(-wallThickness, 0, 0);
+                }
+
+                //scale and move wall to make place for door
+                walls[randomWall].transform.position += new Vector3(randomPlaceOnWall / 2 + .5f, 0, 0);
+                float restWallToFill = walls[randomWall].transform.localScale.x - (walls[randomWall].transform.localScale.x - randomPlaceOnWall);
+                Debug.Log(restWallToFill);
+                walls[randomWall].transform.localScale -= new Vector3(randomPlaceOnWall + 1, 0, 0);
+
+                //add new wall on empty space between doors and other wall
+                {
+                    if(randomPlaceOnWall != 0)
+                    {
+                        walls.Add(Instantiate(wallPrefab));
+                        walls[walls.Count - 1].transform.rotation = walls[randomWall].transform.rotation;
+                        walls[walls.Count - 1].transform.localScale = new Vector3(restWallToFill, wallHeight, wallThickness);
+                        walls[walls.Count - 1].transform.position = walls[randomWall].transform.position;
+                        walls[walls.Count - 1].transform.position -= new Vector3(restWallToFill / 2 + walls[randomWall].transform.localScale.x / 2 + 1, 0, 0);
+                    }
+                }
+            }
+            else
+            {
+                door.transform.position += new Vector3(0, 0, -walls[randomWall].transform.localScale.x / 2 + randomPlaceOnWall + .5f);
+                if (randomPlaceOnWall == 0)
+                {
+                    door.transform.position += new Vector3(0, 0, wallThickness);
+                }
+                else if (randomPlaceOnWall == Mathf.RoundToInt(walls[randomWall].transform.localScale.x))
+                {
+                    door.transform.position += new Vector3(0, 0, -wallThickness);
+                }
+
+                walls[randomWall].transform.position += new Vector3(0, 0, randomPlaceOnWall / 2 + .5f);
+                float restWallToFill = walls[randomWall].transform.localScale.x - (walls[randomWall].transform.localScale.x - randomPlaceOnWall);
+                Debug.Log(restWallToFill);
+                walls[randomWall].transform.localScale -= new Vector3(randomPlaceOnWall + 1, 0, 0);
+
+                {
+                    if (randomPlaceOnWall != 0)
+                    {
+                        walls.Add(Instantiate(wallPrefab));
+                        walls[walls.Count - 1].transform.rotation = walls[randomWall].transform.rotation;
+                        walls[walls.Count - 1].transform.localScale = new Vector3(restWallToFill, wallHeight, wallThickness);
+                        walls[walls.Count - 1].transform.position = walls[randomWall].transform.position;
+                        walls[walls.Count - 1].transform.position -= new Vector3(0, 0, restWallToFill / 2 + walls[randomWall].transform.localScale.x / 2 + 1);
+                    }
+                }
+            }
         }
 
 
